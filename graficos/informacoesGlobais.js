d8af708cfc8a076100d3260ff16daa558b13760e
luisaoliveiraa <105437774+luisaoliveiraa@users.noreply.github.com>
2024-10-18 08:38:00 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,11 +7,11 @@ async function visualizarInformacoesGlobais(){
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_dia_estudando)
     //const minutos = Math.round((dados.tempo_medio_dia_estudando - horas) * 100)
-    const pessoasEstudando = ((pessoasComAcesso / pessoasNoMundo) * 100).toFixed(2)
+    const total_pessoas_que_praticam_esportes_regularmente = ((pessoasComAcesso / pessoasNoMundo) * 100).toFixed(2)
     //console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e que aproximadamente <span>${pessoasEstudando}%</span> delas estão fazendo algum curso e passam em média <span>${horas} horas</span> estudando?`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e que aproximadamente <span>${total_pessoas_que_praticam_esportes_regularmente}%</span> delas estão fazendo algum curso e passam em média <span>${horas} horas</span> estudando?`
     //console.log(paragrafo)
     
     const container = document.getElementById('graficos-container')
