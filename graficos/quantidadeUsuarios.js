80bb03e66a2ec7ae2b7194f23fd1b98ecbf0c835
luisaoliveiraa <105437774+luisaoliveiraa@users.noreply.github.com>
2024-11-08 08:40:35 -0300

    o

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,40 +1,39 @@
 import { getCSS, tickConfig } from "./common.js"
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/luisaoliveiraa/ciencia-de-dados/refs/heads/main/base-de-dados/esportes-mais-praticados.json'
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json&#39;
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasInstituicoes = Object.keys(dados)
-    const quantidadeAlunos = Object.values(dados)
-    
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
     const data = [
         {
-            x: nomeDasInstituicoes,
-            y: quantidadeAlunos,
+            x: nomeDasRedes,
+            y: quantidadeDeUsuarios,
             type: 'bar',
             marker: {
                 color: getCSS('--primary-color')
             }
-
         }
     ]
-    const layout = {
+
+    const laytout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'teste',
+            text: 'Redes sociais com mais usuários',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
-                family: getCSS('--font'),
-                size: 30
+                size: 30,
+                font: getCSS('--font')
             }
-
         },
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Teste',
-                size: 20,
+                text: 'Nome das redes',
                 font: {
                     color: getCSS('--secondary-color')
                 }
@@ -43,18 +42,17 @@ async function quantidadeUsuarios() {
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'teste',
-                size:20,
+                text: 'Bilhões de usuários ativos',
                 font: {
                     color: getCSS('--secondary-color')
                 }
             }
         }
     }
+
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
     Plotly.newPlot(grafico, data, layout)
-
 }
 quantidadeUsuarios();
