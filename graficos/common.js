637c333d18c5d244cf91c0cc94aaecd95c0c0c7e
luisaoliveiraa <105437774+luisaoliveiraa@users.noreply.github.com>
2024-11-08 08:35:11 -0300

    ok

--- graficos/common.js
+++ graficos/common.js
@@ -1,68 +1,11 @@
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
-};
+}
 
 const tickConfig = {
     color: getCSS('--primary-color'),
-    size: 15,
+    size: 16,
     family: getCSS('--font')
 }
-export{ getCSS, tickConfig }
 
-quantidadeUsuarios.js
-import { getCSS, tickConfig } from "./common.js"
-
-async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json&#39;
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeDeUsuarios = Object.values(dados)
-
-    const data = [
-        {
-            x: nomeDasRedes,
-            y: quantidadeDeUsuarios,
-            type: 'bar',
-            marker: {
-                color: getCSS('--primary-color')
-            }
-        }
-    ]
-
-    const laytout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        title: {
-            text: 'Redes sociais com mais usuários',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                size: 30,
-                font: getCSS('--font')
-            }
-        },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Nome das redes',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Bilhões de usuários ativos',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        }
-    }
-
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
+export {getCSS, tickConfig}
