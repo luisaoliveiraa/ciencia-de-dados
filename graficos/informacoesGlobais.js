3a7eabec243cf7fbbf0dfabff3a0cdd9068268cb
luisaoliveiraa <105437774+luisaoliveiraa@users.noreply.github.com>
2024-11-29 07:52:46 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,15 +3,15 @@ const url = 'https://raw.githubusercontent.com/luisaoliveiraa/ciencia-de-dados/r
 async function visualizarInformacoesGlobais(){
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasComAcesso = (dados.total_pessoas_que_praticam_esportes_regularmente/ 1e9)
+    const pessoasPraticam= (dados.total_pessoas_que_praticam_esportes_regularmente/ 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_semana_praticando_esportes)
     //const minutos = Math.round((dados.tempo_medio_semana_praticando_esportes - horas) * 100)
-    const total_pessoas_que_praticam_esportes_regularmente = ((pessoasComAcesso / pessoasNoMundo) * 100).toFixed(2)
+    const total_pessoas_que_praticam_esportes_regularmente = ((pessoasPraticam/ pessoasNoMundo) * 100).toFixed(2)
     //console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e que aproximadamente <span>${total_pessoas_que_praticam_esportes_regularmente}%</span> delas praticam algum esporte e passam em média <span>${horas} horas</span> praticando?`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e que aproximadamente <span>${pessoasPraticam}}%</span> delas praticam algum esporte e passam em média <span>${horas} horas</span> praticando?`
     //console.log(paragrafo)
     
     const container = document.getElementById('graficos-container')
